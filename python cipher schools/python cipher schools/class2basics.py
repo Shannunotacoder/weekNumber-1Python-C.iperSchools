0dcda239711efe5bc6d38ba182edf46e0915042e
Shannunotacoder <109965164+Shannunotacoder@users.noreply.github.com>
2022-12-01 15:11:37 +0530

    Add files via upload

---
+++ python cipher schools/python cipher schools/class2basics.py
@@ -0,0 +1,39 @@
+print("Hello")
+print("Hello World")
+
+# -> comment
+
+# python interpreter is written in C language
+# every line executed in python interpreter is
+#   1 statement 
+#   2 expression
+
+"shanmukh"
+"""shanmukh"""
+
+100 + 200
+_+ 200 # _ is the previous answer
+
+a = "a"
+type(a)
+
+# four bacis data types in python
+# 1 string
+# 2 integer
+# 3 float
+# 4 complex
+
+isinstance("a",int)
+isinstance("b", str)
+
+a = 255
+print(id(a))
+
+a = 200
+print(id(a))
+
+a = 4864121894184121333
+print(a+1)
+
+a=647364264367589709687435442544874643558785456378700
+print(a+2)
---
+++ python cipher schools/python cipher schools/day1.py
@@ -0,0 +1,45 @@
+# print('hello world')
+# print('hello "world" world')
+# print('i\'m shanmukh')
+print("line A")
+print("line B")
+print("line A\nline B")
+print("name\tharshit")
+print("this is \ backslash")
+print("hell\blo")
+
+print("this is \\\\ double backslash")
+print("these are /\\/\\/\\/\\ mountains")
+print("he is\tawesome")
+print(" \\\" \\t \\\' ")
+
+
+print(r"line A \n line B")
+
+#how to print emoji
+print("\U0001F602")
+print("\U0001F601")
+print("\U0001F618")
+
+print(2+3)
+print(4//3)
+print(10%5) # remainder
+print(2**8)
+print(2**0.5)
+print(round(2**0.5, 4))
+
+print((2+3) *5/2%6)
+
+name = "shanmukh"
+print(name)
+name = 1234
+print(name)
+
+# rule one
+# 1number=14
+# letter
+_name = "Shanmukh"
+print(_name)
+
+
+
